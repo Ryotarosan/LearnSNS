5f133922b45de5c9e490666ff4dcba5aadfd654b
Ryotaro <>
2018-08-14 15:32:55 +0800

    うええええええ

--- timeline.php
+++ timeline.php
@@ -11,7 +11,39 @@ require('dbconnect.php');
   
     $signin_user = $stmt->fetch(PDO::FETCH_ASSOC);
     // var_dump($signin_user);
+ 
+    $errors = array();
+if(!empty($_POST )){
+$feed = $_POST['feed']; 
+if($feed==''){
+$errors['feed']='blank';
+}else{
+ $sql = 'INSERT INTO `feeds` SET `feed`=?, `user_id`=?, `created`=NOW()';
+      $data = array($feed, $signin_user['id']);
+      $stmt = $dbh->prepare($sql);
+      $stmt->execute($data);
 
+      header('Location: timeline.php');
+      exit();
+    }
+}
+// post送信されたらというif文の終わり
+    $sql = 'SELECT `f`.*, `u`.`name`, `u`.`img_name` FROM `feeds` AS `f` LEFT JOIN `users` AS `u` ON `f`.`user_id`=`u`.`id` ORDER BY `created` DESC';
+    $data = array();
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
+
+    // 表示用の配列を初期化
+    $feeds = array();
+
+    while (true) {
+        $record = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($record == false) {
+            break;
+        }
+        $feeds[] = $record;
+}
+// var_dump($feeds);
 
 ?>
 <!DOCTYPE html>
@@ -41,22 +73,28 @@ require('dbconnect.php');
             <div class="form-group">
               <textarea name="feed" class="form-control" rows="3" placeholder="Happy Hacking!" style="font-size: 24px;"></textarea><br>
             </div>
+            <?php if (isset($errors['feed']) && $errors['feed'] == 'blank') { ?>
+                <p class="alert alert-danger">投稿データを入力してください</p>
+              <?php } ?>
+
             <input type="submit" value="投稿する" class="btn btn-primary">
           </form>
         </div>
+        <?php foreach($feeds as $feed){ ?>
+
           <div class="thumbnail">
             <div class="row">
               <div class="col-xs-1">
-                <img src="https://placehold.jp/40x40.png" width="40">
+                <img src="user_profile_img/<?php echo $feed['img_name'];?>" width="40">
               </div>
               <div class="col-xs-11">
-                野原ひろし<br>
-                <a href="#" style="color: #7F7F7F;">2018-03-03</a>
+                <?php echo $feed['name']; ?><br>
+                <a href="#" style="color: #7F7F7F;"><?php echo $feed['created']; ?></a>
               </div>
             </div>
             <div class="row feed_content">
               <div class="col-xs-12" >
-                <span style="font-size: 24px;">夢は逃げない。逃げるのはいつも自分だ。</span>
+                <span style="font-size: 24px;"><?php echo $feed['feed']; ?></span>
               </div>
             </div>
             <div class="row feed_sub">
@@ -74,6 +112,7 @@ require('dbconnect.php');
               </div>
             </div>
           </div>
+          <?php } ?>
         <div aria-label="Page navigation">
           <ul class="pager">
             <li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span> Newer</a></li>
